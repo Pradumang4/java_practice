c1616db77b0ffa90ce3a4a856312de22954b932c
Pradumang4 <pradumang4@gmail.com>
2026-08-15 16:33:46 +0530

    More practice

--- src/com/ok/dailyRevise/inheritance.java
+++ src/com/ok/dailyRevise/inheritance.java
@@ -1,22 +1,30 @@
 package com.ok.dailyRevise;
 
-public class inheritance {
-    static class animal{
-        void eat(){
+      class animal{
+     void eat(){
             System.out.println("Animal is eating");
         }
     }
-    static class dog extends animal{
+     class dog extends animal{
         void bark(){
             System.out.println("Dog is barking");
 
         }
     }
+    class cat extends animal{
+          void meow(){
+              System.out.println("Cat is meowing");
+          }
 
+    }
+
+public class inheritance{
     public static void main(String[] args) {
         dog d1=new dog();
         d1.eat();
         d1.bark();
-
+         cat c1= new cat();
+         c1.eat();
+         c1.meow();
     }
 }
